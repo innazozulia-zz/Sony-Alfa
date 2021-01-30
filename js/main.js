59c1a551ff4bb3e7ea0cb787af55ba905d94fa43
Inna <innazozulia@outlook.com>
2021-01-30 14:54:11 +0200

    init version 1

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+  $(".slider").slick({
+    arrows: false,
+    fade: true,
+    autoplay: 2000,
+    dots: true,
+  });
+  $(".header-btn").on("click", function () {
+    $(".menu").addClass("active");
+  });
+  $(".close-btn").on("click", function () {
+    $(".menu").removeClass("active");
+  });
+});
